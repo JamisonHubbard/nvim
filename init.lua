fad3cb558bf3efc5ed05eeb683e0d33655e8c7e6
JamisonHubbard <jhubbard956@gmail.com>
2024-06-16 13:09:17 -0600

    Add lazy.nvim package manager

--- init.lua
+++ init.lua
@@ -1,4 +1,27 @@
+-- tab space config
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+
+-- lazy package manager
+-- check if lazy.nvim already exists at the expected path
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- if not, install lazy.nvim
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable branch
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- create empty variables needed to start lazy.nvim
+local plugins = {}
+local opts = {}
+
+-- call the setup function for lazy.nvim
+require("lazy").setup(plugins, opts)
